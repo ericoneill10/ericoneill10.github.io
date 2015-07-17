0a7936dcc2020205f00b842d7515e1a61ed03d78
Eric O'Neill <ericoneill10@gmail.com>
2015-07-17 16:34:58 -0700

    debug javascript

--- js/placeSearch.js
+++ js/placeSearch.js
@@ -1,17 +1,19 @@
 function initialize() {
+  console.log("a");
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: new google.maps.LatLng(-33.8665433, 151.1956316),
     zoom: 15
   });
 
-  /*
+  console.log("b");
   var request = {
     placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
   };
+  console.log("c");
 
   var infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
-
+console.log("d");
   service.getDetails(request, function(place, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       var marker = new google.maps.Marker({
@@ -24,7 +26,8 @@ function initialize() {
       });
     }
   });
-  */
+  console.log("e");
 }
-
+console.log("f");
 google.maps.event.addDomListener(window, 'load', initialize);
+console.log("g");
